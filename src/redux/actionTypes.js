2cee7975a6c3a1b3b1f6fb3959016991d77d46fe
Boris Joskic <joskicboris95@gmail.com>
2020-11-05 17:41:07 +0100

    Add Redux and Homepage data

---
+++ src/redux/actionTypes.js
@@ -0,0 +1,3 @@
+//TOP NEWS
+export const ADD_TOP_NEWS = "ADD_TODO";
+export const SET_TOP_NEWS_STATUS = "TOGGLE_TODO";
---
+++ src/redux/selectors.js
@@ -0,0 +1,6 @@
+export const getTopNewsState = store => store.topNews;
+
+export const getTopNews = store =>
+	getTopNewsState(store) ? getTopNewsState(store).topNews : [];
+
+export const getTopNewsLoadingStatus = store => getTopNewsState(store).status
