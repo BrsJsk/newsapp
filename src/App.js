f7621cc7f27b790040b1d535c55a6427622bfc39
Boris Joskic <joskicboris95@gmail.com>
2020-11-05 08:16:54 +0100

    React router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import {Home} from "./Home";
 import React from "react";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+} from "react-router-dom";
+import {Header} from "./shared";
+import Categories from "./Categories/Categories";
+import Search from "./Search/Search";
 
 function App() {
 	return (
-		<div className="App">
-			<Home/>
-		</div>
+		<Router>
+			<div>
+				<Header/>
+
+				<Switch>
+					<Route path="/search">
+						<Search />
+					</Route>
+					<Route path="/categories">
+						<Categories	/>
+					</Route>
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
 	);
 }
 
