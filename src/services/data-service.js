100cca9ac5ef40ae540a382b707a1f0a900dd3f0
Boris Joskic <joskicboris95@gmail.com>
2020-11-08 20:31:06 +0100

    Category Top 5 News data

--- src/services/data-service.js
+++ src/services/data-service.js
@@ -15,3 +15,11 @@ export const loadTopNewsForCategory = (category, country = Countries.GREAT_BRITA
     `${REACT_APP_API_URL}top-headlines?country=${country}&category=${category}&apiKey=${REACT_APP_API_KEY}`,
   ).then((res) => res.json());
 };
+
+export const loadTop5NewsForCategory = (category, country = Countries.GREAT_BRITAIN) => {
+  const { REACT_APP_API_KEY, REACT_APP_API_URL } = process.env;
+
+  return fetch(
+    `${REACT_APP_API_URL}top-headlines?country=${country}&category=${category}&pageSize=5&apiKey=${REACT_APP_API_KEY}`,
+  ).then((res) => res.json());
+};
