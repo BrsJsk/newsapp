44e6e16ce113645569a5f4fe6fcd8b50296e2943
Boris Joskic <joskicboris95@gmail.com>
2020-11-09 16:28:08 +0100

    Header responsive

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -5,6 +5,7 @@ import { getIsCountryDisabled, getSelectedCountry } from '../redux/selectors';
 import { connect } from 'react-redux';
 import { SetCountry, SetIsDisabledCountry } from '../redux/actions';
 import { Countries } from '../constants';
+import { device } from '../styles/breakpoints';
 
 function Header(props) {
   const { country, isDisabled, SetCountry, SetIsDisabledCountry } = props;
@@ -25,19 +26,21 @@ function Header(props) {
 
   return (
     <HeaderWrapper>
-      <div>
-        <Link to="/">
-          <HeaderButton>Top News</HeaderButton>
-        </Link>
-        <Link to="/categories">
-          <HeaderButton>Categories</HeaderButton>
-        </Link>
-        <Link to="/search">
-          <HeaderButton>Search</HeaderButton>
-        </Link>
-      </div>
+      <ButtonRow>
+        <HeaderButtonTW to="/" active={location.pathname === '/'}>
+          Top News
+        </HeaderButtonTW>
 
-      <div>
+        <HeaderButtonTW to="/categories" active={location.pathname === '/categories'}>
+          Categories
+        </HeaderButtonTW>
+
+        <HeaderButtonTW to="/search" active={location.pathname === '/search'}>
+          Search
+        </HeaderButtonTW>
+      </ButtonRow>
+
+      <ButtonRow>
         <HeaderButton
           onClick={() => setActiveCountry(Countries.GREAT_BRITAIN)}
           disabled={isDisabled}
@@ -52,22 +55,57 @@ function Header(props) {
         >
           US
         </HeaderButton>
-      </div>
+      </ButtonRow>
     </HeaderWrapper>
   );
 }
 
 const HeaderWrapper = styled.div`
   display: flex;
-  height: 60px;
   background: #fbf6f6;
   justify-content: space-between;
+
+  @media ${device.mobileS} {
+    height: auto;
+    flex-direction: column;
+  }
+
+  @media ${device.tablet} {
+    height: 60px;
+    flex-direction: row;
+  }
+`;
+
+const ButtonRow = styled.div`
+  display: flex;
+  @media ${device.mobileS} {
+    flex-direction: column;
+  }
+
+  @media ${device.tablet} {
+    flex-direction: row;
+  }
+`;
+
+const HeaderButtonTW = styled(Link)`
+  background: ${(props) => (props.active ? '#6161aa' : '#fbf6f6')};
+  color: ${(props) => (props.active ? '#f3f0f0' : '#1d1c1c')};
+  display: flex;
+  align-items: center;
+  min-width: 120px;
+  justify-content: center;
+  outline: none;
+  text-decoration: none;
+  font-size: 14px;
+  border: 1px solid #a4a4a9;
+  height: 60px;
 `;
 
 const HeaderButton = styled.button`
   height: 60px;
   min-width: 120px;
   background: ${(props) => (props.active ? '#6161aa' : '#fbf6f6')};
+  color: ${(props) => (props.active ? '#f3f0f0' : '#1d1c1c')};
   opacity: ${(props) => (props.disabled ? 0.5 : 1)};
   cursor: ${(props) => (props.disabled ? 'not-allowed' : 'pointer')};
   border: 1px solid #a4a4a9;
